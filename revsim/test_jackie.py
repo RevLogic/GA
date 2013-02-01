bc9412c768ee52c44f7a1bb39e52355b20c331c4
Christopher Rabl <rabl.christopher@gmail.com>
2013-02-01 11:50:50 -0700

    Forgot to re-add Jackie's test...

---
+++ revsim/test_jackie.py
@@ -0,0 +1,17 @@
+import sys
+from revsim import *
+
+# Implements Jackie's one-bit adder detailed in class
+
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+lines = [a, b, 0, 0]
+
+c = Cascade(lines)
+c.append(tof, [0, 1], 2)
+c.append(tof, [0], 3)
+c.append(tof, [1], 3)
+output = c.run(True)
+
+print "CAR: ", output[2]
+print "SUM: ", output[3]
