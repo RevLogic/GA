efd06f33f16e6b0ccfac32410949a3d479cf2439
Christopher Rabl <rabl.christopher@gmail.com>
2013-02-07 01:18:19 -0700

    Added some testing code for binary permutations in test_universal.py

--- revsim/test_universal.py
+++ revsim/test_universal.py
@@ -7,18 +7,21 @@ line_perms = perms.binary_iterator(3)
 c = Cascade([0, 0, 0])
 c.append(tof, [0,1], 2)
 
-#d = Cascade([0, 0, 0])
-#d.append(tof, [0,1], 2)
+d = Cascade([0, 0, 0])
+d.append(fred, [1], [0,2])
+d.append(fred, [2], [0,1])
+d.append(fred, [0], [1,2])
 
 # Run through every input permutation and output
 # the gate results at every step
 for perm in line_perms:
     c.replace_lines(perm)
-    #d.replace_lines(perm)
+    d.replace_lines(perm)
     print c.run()
-    print c.run()
-    #if c.run() != d.run():
-        #print "NO"
+    print d.run()
+    if c.run() != d.run():
+        print "NO"
+        exit()
     print ""
 
 
