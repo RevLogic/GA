199db32fa4fcce9a1730dff2fbdb965c40ca6540
Christopher Rabl <rabl.christopher@gmail.com>
2013-02-22 15:13:05 -0700

    Now THAT'S what I call a test file...

--- revsim/test_massive.py
+++ revsim/test_massive.py
@@ -21,12 +21,26 @@ print "Quantum Cost:", c.quantum_cost()
 
 d = c.copy()
 print "D quantum Cost:", d.quantum_cost()
+
 # DO NOT RUN THIS UNLESS YOU HAVE OVER A GIG OF RAM TO SPARE !!!!!
+"""
 print ""
-print "Loading 1000 Cascades into RAM..."
+print "Copying 1000 Cascades from RAM..."
 cascadeList = []
 for i in range(0, 1000):
     cascadeList.append(c.copy())
+    print i
+"""
+
+# DO NOT RUN THIS UNLESS YOU HAVE EVEN MORE RAM TO SPARE !!!!!
+"""
+print ""
+print "Loading 1000 Cascades into RAM from disk..."
+cascadeList2 = []
+for i in range(0, 1000):
+    cascadeList2.append(Cascade(lines, "massive.pckl"))
+    print i
+"""
 
 # This part took about 4 minutes to run on linux1... not bad
 print ""
