c5f487e44a4b2d57ed062314845c88abf880638a
Christopher Rabl <rabl.christopher@gmail.com>
2013-03-18 21:37:05 -0600

    Added all Cascade functions to beta, now every gate works

--- revsim/experimental/cascade.py
+++ revsim/experimental/cascade.py
@@ -4,10 +4,42 @@ from lines import *
 class Cascade:
     gate_list = []
     lines = None
+    cost = 0
 
-    def __init__(self):
-        
+    def __init__(self, lines):
+        self.lines = lines.copy()
+
+    def __len__(self):
+        return len(self.gate_list)
 
     def run(self):
-        for gate in gate_list:
-            self.lines = gate.eval(self.lines)
+        for gate in self.gate_list:
+            gate.eval(self.lines)
+        return self.lines
+    
+    def insert(self, gate, pos):
+        """
+        Insert a function, control-list, and target into the current Cascade's
+        gate-list, control-list, and target-list
+        """
+        if pos > len(self) or pos < 0:
+            raise ValueError
+
+        self.gate_list.insert(pos, gate)
+        # self.cost += gate.quantum_cost() # TODO: make sure this works
+
+    def append(self, gate):
+        """
+        Append the current function, control list, and target to the
+        gate-list, control-list, and target-list.
+        NOTE: This does NOT evaluate the function on the list
+        """
+        self.insert(gate, len(self))
+
+    def prepend(self, gate):
+        """
+        Prepend the current function, control list, and target to the
+        gate-list, control-list, and target-list.
+        NOTE: As with append, this does NOT evaluate the Cascade on the list
+        """
+        self.insert(gate, 0)
