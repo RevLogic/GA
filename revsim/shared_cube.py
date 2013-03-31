82ee9d5ad880ef10a717bd739bb4d2796917bb50
Chris Rabl <rabl.christopher@gmail.com>
2013-03-31 01:05:15 -0400

    Changed terminology

--- revsim/shared_cube.py
+++ revsim/shared_cube.py
@@ -2,21 +2,27 @@
 Given a Cascade, output its cube-list and a new Cascade which
 has the same function, but has been reduced using the method
 of Nayeem-Rice.
+
+Note that in order to generate the shared-cube representation
+of a Cascade, it is required that Cascades have designated
+"output lines" that are initialized to 0. These lines may be 
+targets and controls to gates. Any line that is NOT an output
+line MUST NOT BE USED AS A TARGET.
 """
 from revsim import *
 
 class SharedCube:
     def __init__(self, cascade):
         self.c = cascade
-        self.not_garbage = cascade.constant_line_labels() # change this
+        self.output_lines =  cascade.constant_line_labels() # change this
 
     def generate(self):
         # this code is horrible
         cube_list = {}
 
-        for gate in self.c:
+        for gate in self.c: 
             target = gate.get_target()
-            if target in self.not_garbage:
+            if target in self.output_lines:
                 controls = tuple(gate.get_controls())
                 try:
                     cube_list[controls].append(target)
@@ -26,7 +32,7 @@ class SharedCube:
         # select all cubes shared by a given key
         shared_key_values = {}
         for key in cube_list:
-            if key[0] in self.not_garbage:
+            if key[0] in self.output_lines:
                 shared_key_values[key] = cube_list[key]
         
         for item in shared_key_values:
