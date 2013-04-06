24623d43b9986c94179724d6a1b4516b49c9c772
Chris Rabl <rabl.christopher@gmail.com>
2013-04-06 02:15:01 -0600

    GA params still need tuning, but overall, it's pretty good

--- revsim/smart_ga.py
+++ revsim/smart_ga.py
@@ -92,7 +92,7 @@ class SmartGA(GeneticAlgorithm):
                 self.population += self.crossover(top_two[0][1], top_two[1][1])
                 current_fitness = new_fitness
             else:
-                self.generate_population(self.max_population_size, 5)
+                self.generate_population(self.max_population_size, 3)
                 self.population += [top_two[0][1], top_two[1][1]]
 
             if (gen_count == self.max_generations - 1) or (current_fitness == 1.0):
